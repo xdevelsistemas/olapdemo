bdb6b13b439ba6d8e7fabf29876177e60de8587f
pnakibar <pedro.nakibar@xdevel.com.br>
2015-12-30 15:21:41 -0200

    convertendo para json

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,26 @@
 package controllers
 
 import java.io.PrintWriter
+import java.util.Date
 
+import com.fasterxml.jackson.databind.{SerializationConfig, ObjectMapper}
 import org.olap4j.CellSet
 import org.olap4j.layout.RectangularCellSetFormatter
+import org.saiku.olap.util.OlapResultSetUtil
+import org.saiku.olap.util.formatter.FlattenedCellSetFormatter
+import org.saiku.web.rest.objects.resultset.QueryResult
+import org.saiku.web.rest.util.RestUtil
 import play.api._
 import play.api.mvc._
+import spray.json._
+import DefaultJsonProtocol._
+
+
+
 object Application extends Controller {
 
   def index = Action {
+    println("opa")
     val l : Array[String] = Array.empty
     val xconn = lib.io.gerenc.xDevOlapConnection.conn
 
@@ -19,18 +31,29 @@ object Application extends Controller {
     // Prepare a statement.
     val cellSet: CellSet = xconn.createStatement.executeOlapQuery(query)
 
-    // We use the utility formatter.
-    val formatter: RectangularCellSetFormatter = new RectangularCellSetFormatter(false)
 
-    // Print out.
-    val writer: PrintWriter = new PrintWriter(System.out)
-    formatter.format(cellSet, writer)
-    writer.flush()
+    /*de trecho baixo foi inspirado de:
+      * org.saiku.service.olap.OlapQueryService.{executeMdx, execute}
+      * e
+      * org.saiku.web.rest.resources.QueryResource.executeMdx
+      *
+      * Que é como eles lidam com a o CellSet, trazendo para dentro do saiku, utilizando as estruturas deles
+      */
+    val formatter = new FlattenedCellSetFormatter()
+
+
+    val result = OlapResultSetUtil.cellSet2Matrix(cellSet)
+    val queryResult = RestUtil.convert(result)
 
 
 
 
-    Ok(views.html.index("Your new application is ready."))
+
+    // conversão simples com o jackson
+    val mapper = new ObjectMapper()
+    val json = mapper.writeValueAsString(queryResult)
+
+    Ok(json)
   }
 
 }
